d41447c421d6c9fac7c3a99b1d3596c2297f0945
Prajwal <prajwal1510j@gmail.com>
2025-07-15 15:43:04 +0530

    01:setup

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button"
+
+const page = () => {
+  return (
+    <div>
+      Hello World
+      <Button variant='destructive'>
+        Click me
+      </Button>
+    </div>
+  )
+}
+
+export default page

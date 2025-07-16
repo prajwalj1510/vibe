3a367ac43049003070d55dc09f8ee29ddd7cf347
Prajwal <prajwal1510j@gmail.com>
2025-07-16 10:36:53 +0530

    03:trpc-setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,44 @@
-
+// "use client"
 import { Button } from "@/components/ui/button"
+// import { useTRPC } from "@/trpc/client"
+// import { useQuery } from "@tanstack/react-query"
+
+// import { caller } from "@/trpc/server"
+
+import { Suspense } from "react"
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query"
+import { getQueryClient, trpc } from "@/trpc/server"
+import {Client} from './client'
 
 const page = async() => {
-  
+
+  // Client side TRPC fetching
+  // const trpc = useTRPC()
+  // // trpc.hello.queryOptions({ text: 'Hello' })
+  // const {data} = useQuery(trpc.hello.queryOptions({text: 'Prajwal'}))
+
+  // const data = await caller.hello({"text":"Prajwal"})
+
+  const queryClient = getQueryClient()
+  void queryClient.prefetchQuery(trpc.hello.queryOptions({text:"Prajwal Prefetch"}))
+
   return (
     <div>
-      Hello World
-      
+      {/* Hello World
+        Client Component 
       <Button variant='destructive'>
-        Click me
-      </Button>
+        Click me {JSON.stringify(data)}
+      </Button> */}
+
+      {/* {JSON.stringify(data)} */}
+
+      <HydrationBoundary state={dehydrate(queryClient)}>
+          <Suspense fallback={<p>Loading....</p>}>
+            <Client/>
+          </Suspense>
+      </HydrationBoundary>
+
+
     </div>
   )
 }
